296ef6822bb33957ce34130ecdfb8734df8a883e
Miha <lv11miha@gmail.com>
2025-12-06 03:39:13 +0200

    added news

--- src/api/news.js
+++ src/api/news.js
@@ -1,5 +1,5 @@
 const news = async (page) => {
-  const response = await fetch(`http://localhost:5000/news/${page}`);
+  const response = await fetch(`https://forecaster-back-96pv.onrender.com/news/${page}`);
   const data = await response.json();
 
   return data;
